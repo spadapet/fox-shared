e414bc0f185a1e865b3814165a21f6a42fd09a05
Peter Spada <spadapet@hotmail.com>
2023-12-22 14:29:35 -0800

    Start tiled levels

---
+++ assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="30" tileheight="18" tilecount="240" columns="16">
+ <image source="tileset.png" width="480" height="270"/>
+</tileset>
