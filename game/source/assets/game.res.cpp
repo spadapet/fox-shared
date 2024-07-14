a183b4bfdd80147d0c7608cf71724e35b9f66e04
Peter Spada <spadapet@hotmail.com>
2024-07-13 23:14:47 -0700

    Update ff_game_library

--- game/source/assets/game.res.cpp
+++ game/source/assets/game.res.cpp
@@ -5,6 +5,7 @@ namespace res
 {
     void register_game()
     {
-        ff::global_resources::add(::assets::game::data());
+        ff::data_reader assets_reader(::assets::game::data());
+        ff::global_resources::add(assets_reader);
     }
 }
