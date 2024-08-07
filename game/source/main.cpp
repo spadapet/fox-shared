468f3ab06766aa378465b96b88fabf3083311ffe
Peter Spada <spadapet@hotmail.com>
2024-08-07 09:42:59 -0700

    Auto load resource packs

--- game/source/main.cpp
+++ game/source/main.cpp
@@ -4,16 +4,7 @@
 
 static void register_global_resources()
 {
-    // TODO: Make helper method to automatically load all assets from EXE's directory
-    std::filesystem::path assets_path = ff::filesystem::executable_path().parent_path() / "game.res.pack";
-    std::shared_ptr<ff::data_base> assets_data = ff::filesystem::map_binary_file(assets_path);
-    assert(assets_data);
-
-    if (assets_data)
-    {
-        ff::data_reader assets_reader(assets_data);
-        ff::global_resources::add(assets_reader);
-    }
+    verify(ff::global_resources::add_files(ff::filesystem::executable_path().parent_path()));
 
     Noesis::RegisterComponent<game::title_page>();
     Noesis::RegisterComponent<game::title_page_view_model>();
